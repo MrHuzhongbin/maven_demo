5c9c185eccc093a8b31eadda73f7f256b69e2d5b
huzhongbin <18702327006@163.com>
2021-05-27 20:26:22 +0800

    Initial commit

--- src/main/java/com/ithy/config/MysqlConfig.java
+++ src/main/java/com/ithy/config/MysqlConfig.java
@@ -2,8 +2,19 @@ package com.ithy.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class MysqlConfig {
     private DruidDataSource getDataSource(){
+        try {
+            InputStream resourceAsStream = MysqlConfig.class.getClassLoader().getResourceAsStream("mysql.properties");
+            Properties properties = new Properties();
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
